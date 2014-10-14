b47136ccb2cc7403f392ff7faa23f1132f79af18
Michael Stephens <michaelstephens1993@gmail.com>
2014-10-14 18:03:46 +0100

    fixed weird shininess glare

--- demo/reprise_fs.glsl
+++ demo/reprise_fs.glsl
@@ -12,8 +12,8 @@ vec3 calculateColour(Light light_, vec3 V_);
 
 uniform vec3 camPosition;
 
-uniform vec3 materialColour[4];
-uniform float shininess[4];
+uniform vec3 materialColour[7];
+uniform float shininess[7];
 
 uniform Light lights[5];
 
@@ -46,18 +46,24 @@ vec3 calculateColour(Light light_, vec3 V_)
 
 	if(LDdotML > cos(light_.half_cone_angle_degrees))
 	{
-		vec3 R = reflect(-L, vs_normal);
+		vec3 R = normalize(reflect(-L, vs_normal));
 
 		float distance = distance(vs_pos, light_.position);
-		float attenuation = 1.0 / (2
+
+        vec3 attenuatedDistance = vec3(1.0, 1.0, 1.0) * smoothstep(light_.range, 1, distance);
+        vec3 attenuatedCone = vec3(1.0, 1.0, 1.0) * smoothstep(cos(light_.half_cone_angle_degrees), 1, LDdotML);
+
+        vec3 attenuatedLight = attenuatedDistance * attenuatedCone;
+
+		/*float attenuation = 1.0 / (2
 							   + 0.01 * distance
-							   + 0.0001 * distance * distance);
-		vec3 Id = materialColour[vs_matIndex] * max(dot(L, vs_normal), 0) * attenuation;
+							   + 0.0001 * distance * distance);*/
+		vec3 Id = materialColour[vs_matIndex] * max(dot(L, vs_normal), 0) * attenuatedLight;
 
 		vec3 Is = vec3(0, 0, 0);
 		if(dot(L, vs_normal) > 0 && shininess[vs_matIndex] > 0)
 		{
-			Is = vec3(1, 1, 1) * pow(max(0, dot(R, V_)), shininess[vs_matIndex]) * attenuation;
+			Is = vec3(1, 1, 1) * pow(max(0, dot(R, V_)), shininess[vs_matIndex]) * attenuatedLight;
 		}
 
 		return Id + Is;
